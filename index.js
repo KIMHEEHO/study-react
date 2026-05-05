b7782459c4de990ebd87d3339acf14109290686c
희호 <131002889+KIMHEEHO@users.noreply.github.com>
2026-05-05 22:14:19 +0900

    refactor : 컴포넌트 분리

--- index.js
+++ index.js
@@ -3,6 +3,40 @@ console.clear();
 import React, { useState, useRef } from "https://cdn.skypack.dev/react";
 import ReactDOM from "https://cdn.skypack.dev/react-dom";
 
+function TodoApp({todos, addTodo, removeTodo, modifyTodo}) {
+
+  // 추가 버튼 클릭 시 추가 함수 실행
+  const onBtnAddTodoClick = () => {
+    addTodo("리액트 공부");
+  };
+
+  // 삭제 버튼 클릭 시 삭제 함수 실행
+  const onBtnDeleteTodoClick = () => {
+    removeTodo(1);
+  };
+
+  // 수정 버튼 클릭 시 삭제 함수 실행
+  const onBtnModifyTodoClick = () => {
+    modifyTodo(1, "정처기 공부");
+  };
+  
+  return (
+    <>
+      <button onClick={onBtnAddTodoClick}>추가</button>
+      <button onClick={onBtnModifyTodoClick}>수정</button>
+      <button onClick={onBtnDeleteTodoClick}>삭제</button>
+      <h1>todos:</h1>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo.id} {todo.regDate} {todo.content}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
 function App() {
   const [todos, setTodos] = useState([]);
   const lastTodoIdRef = useRef(0);
@@ -35,34 +69,14 @@ function App() {
     setTodos(newTodos);
   };
 
-  // 추가 버튼 클릭 시 추가 함수 실행
-  const onBtnAddTodoClick = () => {
-    addTodo("안녕");
-  };
-
-  // 삭제 버튼 클릭 시 삭제 함수 실행
-  const onBtnDeleteTodoClick = () => {
-    removeTodo(1);
-  };
-
-  // 수정 버튼 클릭 시 수정 함수 실행
-  const onBtnModifyTodoClick = () => {
-    modifyTodo(1, "ㅋㅋㅋ");
-  };
-
   return (
     <>
-      <button onClick={onBtnAddTodoClick}>추가</button>
-      <button onClick={onBtnDeleteTodoClick}>삭제</button>
-      <button onClick={onBtnModifyTodoClick}>수정</button>
-      <hr />
-      <ul>
-        {todos.map((todo, index) => (
-          <li key={index}>
-            {todo.id} {todo.regDate} {todo.content}
-          </li>
-        ))}
-      </ul>
+     <TodoApp
+        todos={todos}
+        addTodo={addTodo}
+        modifyTodo={modifyTodo}
+        removeTodo={removeTodo}
+      />
     </>
   );
 }
