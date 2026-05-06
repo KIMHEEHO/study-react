78ddf01fb415af8e0c78015ae37732845acb1ecd
희호 <131002889+KIMHEEHO@users.noreply.github.com>
2026-05-06 23:20:26 +0900

    feat: 할 일 수정 기능 구현

--- index.js
+++ index.js
@@ -5,9 +5,36 @@ import ReactDOM from "https://cdn.skypack.dev/react-dom";
 
 // 할 일 아이템
 function TodoListItem({ todosState, todo, index }) {
+const [editMode, setEditMode] = useState(false);
+  const [editedContent, setEditedContent] = useState(todo.content);
+  const editedContentInputRef = useRef(null);
+  
   // 할 일 삭제
   const removeTodo = () => {
     todosState.removeTodo(index);
+  };
+
+  // 할 일 수정
+  const showEditMode = () => {
+    setEditMode(true);
+  };
+
+  // 수정 완료
+  const commitEdit = () => {
+    if (editedContent.trim().length == 0) {
+      alert("할 일을 입력해 주세요");
+      editedContentInputRef.current.focus();
+      return;
+    }
+    
+    todosState.modifyTodo(index, editedContent.trim());
+    setEditMode(false);
+  };
+  
+  // 수정 취소
+  const cancelEdit = () => {
+    setEditMode(false);
+    setEditedContent(todo.content);
   };
 
   return (
@@ -17,9 +44,27 @@ function TodoListItem({ todosState, todo, index }) {
         &nbsp;
         {todo.regDate}
         &nbsp;
-        {todo.content}
-        &nbsp;
-        <button onClick={removeTodo}>삭제</button>
+        {editMode || (
+          <>
+            {todo.content}
+            &nbsp;
+            <button onClick={showEditMode}>수정</button>
+            <button onClick={removeTodo}>삭제</button>
+          </>
+        )}
+        {editMode && (
+          <>
+            <input
+              ref={editedContentInputRef}
+              type="text"
+              placeholder="할 일을 입력해 주세요."
+              value={editedContent}
+              onChange={(e) => setEditedContent(e.target.value)}
+            />
+            <button onClick={commitEdit}>수정완료</button>
+            <button onClick={cancelEdit}>수정취소</button>
+          </>
+        )}
       </li>
     </>
   );
